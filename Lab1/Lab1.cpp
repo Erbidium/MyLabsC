dce0143e310fea1336cd54690866379bda0e777b
Oleksandr Tanasiienko <OlexandrTanasienko@gmail.com>
2020-10-21 00:30:32 +0300

    added commentaries

--- Lab1/Lab1.cpp
+++ Lab1/Lab1.cpp
@@ -7,15 +7,16 @@ using namespace std;
 int main()
 {
 	setlocale(LC_ALL,"Russian");
-	double catet1, catet2, innerRadius, hypotenuse;
-	cout<<"Введите катет: ";
-	cin>>catet1;
-	cout<<"Введите гипотенузу: ";
-	cin>>hypotenuse;
-	catet2=sqrt(pow(hypotenuse, 2)-pow(catet1, 2));
-	innerRadius =(catet1+catet2-hypotenuse)/2;
+	double catet1, catet2;										//оголошення катетів
+	double innerRadius, hypotenuse;								//оголошення гіпотенузи та внутрішнього радіусу
+	cout<<"Введите катет: ";									
+	cin>>catet1;												//введення першого катета
+	cout<<"Введите гипотенузу: ";								
+	cin>>hypotenuse;											//введення гіпотенузи
+	catet2=sqrt(pow(hypotenuse, 2)-pow(catet1, 2));	//обчислення другого катета
+	innerRadius =(catet1+catet2-hypotenuse)/2;					//обчислення внутрішнього радіусу трикутника
 	cout<<"Внутренний радиус равен ";
-	printf("%0.3f\n", innerRadius);
+	printf("%0.3f\n", innerRadius);						//виведення радіусу з точністю 3 знаки після крапки
 	system("pause");
 	return 0;
 }
