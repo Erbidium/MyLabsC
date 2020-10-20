f9266ec8f7060100af2511a1631124dd1053831b
Oleksandr Tanasiienko <OlexandrTanasienko@gmail.com>
2020-10-21 00:02:54 +0300

    test version of lab

--- Lab1/Lab1.cpp
+++ Lab1/Lab1.cpp
@@ -1,6 +1,21 @@
 ﻿#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
 
 int main()
 {
-	std::cout << "Hello World!\n";
+	setlocale(LC_ALL,"Russian");
+	double catet1, catet2, innerRadius, hypotenuse;
+	cout<<"Введите катет: ";
+	cin>>catet1;
+	cout<<"Введите гипотенузу: ";
+	cin>>hypotenuse;
+	catet2=sqrt(pow(hypotenuse, 2)-pow(catet1, 2));
+	innerRadius =(catet1+catet2-hypotenuse)/2;
+	
+	cout<<"Внутренний радиус равен"<<setprecision(10)<<innerRadius;
+	system("pause");
+	return 0;
 }
