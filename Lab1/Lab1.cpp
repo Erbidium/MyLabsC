0483addf7986d20670de9fd527da31d7d2c0da77
Oleksandr Tanasiienko <OlexandrTanasienko@gmail.com>
2020-10-21 14:44:54 +0300

    Cleared code and added checks

--- Lab1/Lab1.cpp
+++ Lab1/Lab1.cpp
@@ -5,17 +5,23 @@ using namespace std;
 
 int main()
 {
-	setlocale(LC_ALL,"Russian");		//Зміна локалі, щоб мати можливість виводити кириличні символи
-	double katet1, katet2;							//Оголошення катетів
-	double hypotenuse, innerRadius;					//Оголошення гіпотенузи та радіса кола, вписаного у цей прямокутний трикутник
-	cout<<"Введите катет: ";						//Повідомлення про необхідність ввести значення одного з катетів прямокутного трикутника
-	cin>>katet1;									//Введення першого катета
-	cout<<"Введите гипотенузу: ";					//Повідомлення про необхідність ввести значення гіпотенузи		
-	cin>>hypotenuse;								//Введення гіпотенузи
-	katet2=sqrt(pow(hypotenuse, 2)-pow(katet1, 2));	//Обчислення другого катета
-	innerRadius =(katet1+katet2-hypotenuse)/2;		//Обчислення радіуса
-	cout<<"Радиус окружности, вписанной в этот треугольник: ";//Виведення повідомлення про вивід радіуса
-	printf("%0.3f\n", innerRadius);			//Виведення значення радіуса з точністю 3 знаки після крапки
-	system("pause");
+	setlocale(LC_ALL,"Russian");	
+	double katet1, katet2;
+	double hypotenuse, innerRadius;
+	cout<<"Введите катет: ";		
+	cin>>katet1;
+	if(katet1<=0) cout<<"Треугольник с таким катетом не существует";
+	else{
+		cout<<"Введите гипотенузу: ";			
+		cin>>hypotenuse;
+		if((hypotenuse<=0)||(hypotenuse<katet1))  cout<<"Такой прямоугольный треугольник не существует";
+		else{
+			katet2=sqrt(pow(hypotenuse, 2)-pow(katet1, 2));
+			innerRadius =(katet1+katet2-hypotenuse)/2;
+			cout<<"Радиус окружности, вписанной в этот треугольник: ";
+			printf("%0.3f\n", innerRadius);
+			system("pause");
+			}
+		}
 	return 0;
 }
