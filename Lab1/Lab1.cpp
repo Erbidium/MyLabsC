24611c00b3b4e724c2d9f0e827adf94e4d0c48b6
Oleksandr Tanasiienko <OlexandrTanasienko@gmail.com>
2020-10-21 15:34:48 +0300

    Added new checks

--- Lab1/Lab1.cpp
+++ Lab1/Lab1.cpp
@@ -6,15 +6,16 @@ using namespace std;
 int main()
 {
 	setlocale(LC_ALL,"Russian");	
-	double katet1, katet2;
-	double hypotenuse, innerRadius;
+	double katet1, katet2, hypotenuse, innerRadius;
 	cout<<"Введите катет: ";		
 	cin>>katet1;
-	if(katet1<=0) cout<<"Треугольник с таким катетом не существует";
+	if(cin.fail()) cout<<"Данные введены некоректно!";
+	else if(katet1<=0) cout<<"Треугольник с таким катетом не существует";
 	else{
 		cout<<"Введите гипотенузу: ";			
 		cin>>hypotenuse;
-		if(hypotenuse<katet1)  cout<<"Такой прямоугольный треугольник не существует";
+		if(cin.fail()) cout<<"Данные введены некоректно!";
+		else if(hypotenuse<katet1)  cout<<"Такой прямоугольный треугольник не существует";
 		else{
 			katet2=sqrt(pow(hypotenuse, 2)-pow(katet1, 2));
 			innerRadius =(katet1+katet2-hypotenuse)/2;
