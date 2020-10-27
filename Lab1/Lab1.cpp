5ec16ac1d4f2765f705f32fda7551b6336b6cb52
Oleksandr Tanasiienko <OlexandrTanasienko@gmail.com>
2020-10-28 01:50:34 +0200

    Edited coments

--- Lab1/Lab1.cpp
+++ Lab1/Lab1.cpp
@@ -4,22 +4,21 @@
 using namespace std;
 
 int main()
-{
-	setlocale(LC_ALL,"Russian");	
+{	
 	double katet1, katet2, hypotenuse, innerRadius;
-	cout<<"Введите катет: ";		
+	cout<<"Enter a cathetus: ";		
 	cin>>katet1;
-	if(cin.fail()) cout<<"Данные введены некоректно!";
-	else if(katet1<=0) cout<<"Треугольник с таким катетом не существует";
+	if(cin.fail()) cout<<"Your data is incorrect!";
+	else if(katet1<=0) cout<<"The triangle with such cathetus doesn't exist!";
 	else{
-		cout<<"Введите гипотенузу: ";			
+		cout<<"Enter a hypotenuse: ";			
 		cin>>hypotenuse;
-		if(cin.fail()) cout<<"Данные введены некоректно!";
-		else if(hypotenuse<katet1)  cout<<"Такой прямоугольный треугольник не существует";
+		if(cin.fail()) cout<<"'Your data is incorrect!'";
+		else if(hypotenuse<katet1)  cout<<"Such right triangle doesn't exist!";
 		else{
 			katet2=sqrt(pow(hypotenuse, 2)-pow(katet1, 2));
 			innerRadius =(katet1+katet2-hypotenuse)/2;
-			cout<<"Радиус окружности, вписанной в этот треугольник: ";
+			cout<<"The radius of the circle inscribed in this triangle:";
 			printf("%0.2f\n", innerRadius);
 			system("pause");
 			}
