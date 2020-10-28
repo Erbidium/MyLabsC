8a0c900b192dc031a827533eb3e4534cc97eeac0
Oleksandr Tanasiienko <OlexandrTanasienko@gmail.com>
2020-10-28 14:30:05 +0200

    Edited code of the lab2

--- Lab2/Lab2.cpp
+++ Lab2/Lab2.cpp
@@ -6,6 +6,8 @@ using namespace std;
 int main()
 {
     double x1, y1, x2, y2, x3, y3;
+	const int Precision=3;
+	const int PrecInUse=pow(10, Precision);
 	cout<<"Enter the coordinate x of the first point"<<endl;
 	cin>>x1;
 	if(cin.fail()) cout<<"Your data is incorrect!"<<endl;
@@ -37,9 +39,9 @@ int main()
 						else
 						{
 							float side1, side2, side3;
-							side1=round(sqrt(pow(x1-x2, 2)+pow(y1-y2, 2))*1000)/1000;
-							side2=round(sqrt(pow(x1-x3, 2)+pow(y1-y3, 2))*1000)/1000;
-							side3=round(sqrt(pow(x2-x3, 2)+pow(y2-y3, 2))*1000)/1000;
+							side1=round(sqrt(pow(x1-x2, 2)+pow(y1-y2, 2))*PrecInUse)/PrecInUse;
+							side2=round(sqrt(pow(x1-x3, 2)+pow(y1-y3, 2))*PrecInUse)/PrecInUse;
+							side3=round(sqrt(pow(x2-x3, 2)+pow(y2-y3, 2))*PrecInUse)/PrecInUse;
 							if(((side1+side2)<side3)||((side1+side3)<side2)||((side2+side3)<side1)) cout<<"Such triangle doesn't exist!"<<endl;
 							else if((side1==side2)||(side2==side3)||(side1==side3))
 							{
