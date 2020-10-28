ae5bf970f431df36fd67e81cec9514c96cb4cc4c
Oleksandr Tanasiienko <OlexandrTanasienko@gmail.com>
2020-10-28 04:04:23 +0200

    I created lab 2, wrote the code of the lab2

---
+++ Lab2/Lab2.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    double x1, y1, x2, y2, x3, y3;
+	cout<<"Enter the coordinate x of the first point"<<endl;
+	cin>>x1;
+	if(cin.fail()) cout<<"Your data is incorrect!"<<endl;
+	else
+	{
+		cout<<"Enter the coordinate y of the first point"<<endl;
+		cin>>y1;
+		if(cin.fail()) cout<<"Your data is incorrect!"<<endl;
+		else
+		{
+			cout<<"Enter the coordinate x of the second point"<<endl;
+			cin>>x2;
+			if(cin.fail()) cout<<"Your data is incorrect!"<<endl;
+			else
+			{
+				cout<<"Enter the coordinate y of the second point"<<endl;
+				cin>>y2;
+				if(cin.fail()) cout<<"Your data is incorrect!"<<endl;
+				else
+				{
+					cout<<"Enter the coordinate x of the third point"<<endl;
+					cin>>x3;
+					if(cin.fail()) cout<<"Your data is incorrect!"<<endl;
+					else
+					{
+						cout<<"Enter the coordinate y of the third point"<<endl;
+						cin>>y3;
+						if(cin.fail()) cout<<"Your data is incorrect!"<<endl;
+						else
+						{
+							float side1, side2, side3;
+							side1=round(sqrt(pow(x1-x2, 2)+pow(y1-y2, 2))*1000)/1000;
+							side2=round(sqrt(pow(x1-x3, 2)+pow(y1-y3, 2))*1000)/1000;
+							side3=round(sqrt(pow(x2-x3, 2)+pow(y2-y3, 2))*1000)/1000;
+							if(((side1+side2)<side3)||((side1+side3)<side2)||((side2+side3)<side1)) cout<<"Such triangle doesn't exist!"<<endl;
+							else if((side1==side2)||(side2==side3)||(side1==side3))
+							{
+								if((side1==side2)&&(side2==side3)&&(side1==side3)) cout<<"This triangle is equilateral!"<<endl;//рівносторонній трикутник
+								else {cout<<"This triangle is isosceles!"<<endl;}//рівнобедрений трикутник
+							}
+							else {cout<<"This triangle is versatile!"<<endl;}//різносторонній трикутник
+							
+						}
+						
+					}
+				}
+			}
+		}
+	}
+}
