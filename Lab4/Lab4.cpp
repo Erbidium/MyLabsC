f4d308213059230ae288ea5c968c35f938a57444
Oleksandr Tanasiienko <OlexandrTanasienko@gmail.com>
2020-11-18 17:33:04 +0200

    Edited output

--- Lab4/Lab4.cpp
+++ Lab4/Lab4.cpp
@@ -6,9 +6,10 @@ int main()
 {
 	int n, fact2i=1;
     double x, a;
-	cout<<"Please, enter x and n(natural number) for this sequence"<<endl;
+	cout<<"Please, enter x and n(natural number) to form a sequence"<<endl;
     cin>>x>>n;
 	double xStepi = x;
+	cout<<"The sequence of "<<n<<" numbers(x="<<x<<"):"<<endl;
     for(int i=1;i<=n;i++)
     {
         fact2i=fact2i*(2*i-1)*(2*i);
