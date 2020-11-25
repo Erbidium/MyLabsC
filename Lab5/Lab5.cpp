9a36f09fafdf535d018d7dc9f9efc774f61677a0
Oleksandr Tanasiienko <OlexandrTanasienko@gmail.com>
2020-11-25 09:24:23 +0200

    I wrote code for lab5

---
+++ Lab5/Lab5.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Please, enter natural number n: ";
+    cin>>n;
+    int fact=1;
+    cout<<"A sequence of numbers that fulfill the condition of the problem:\n";
+    for(int i=1;i<=n;i++){
+                fact=fact*i;
+                int j=1;
+                while(j*(j+1)*(j+2)<=fact)
+                {
+                    if(fact==j*(j+1)*(j+2))
+                        {cout<<i<<" ("<<i<<"!="<<j<<"*"<<j+1<<"*"<<j+2<<")\n";}
+                    j++;
+                }
+    }
+    return 0;
+}
