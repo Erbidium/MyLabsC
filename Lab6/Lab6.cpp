b0e01088d3117a9d27df9616fb744f0fda7cacd9
Oleksandr Tanasiienko <OlexandrTanasienko@gmail.com>
2020-12-02 01:55:58 +0200

    Lab6 code

---
+++ Lab6/Lab6.cpp
@@ -0,0 +1,24 @@
+﻿#define _USE_MATH_DEFINES
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+double minimal(double, double);
+
+int main()
+{
+	double a, b, U, V, W;
+	cout<<"Please, enter number a: ";
+	cin>>a;
+	cout<<"Please, enter number b: ";
+	cin>>b;
+	U=minimal(a, b);
+	V=minimal(a*b, a+b);
+	W= minimal(U+pow(V, 2), M_PI);
+}
+
+double minimal(double num1, double num2)
+{
+	return (num1>num2)?num1:num2;
+}
